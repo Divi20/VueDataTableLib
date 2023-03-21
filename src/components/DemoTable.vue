9851430c16dda54a5a8790e00c4fd04c9b35cbb6
Divyangi <divyangisingh@outlook.com>
2023-03-21 11:41:18 +0530

    component exposed - library

---
+++ src/components/DemoTable.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- <tr v-for="(user) in users" :key="user.name">
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.phone }}</td>
+          </tr> -->
+          <tr :key="dhgsvchgdsv">
+            <td>John</td>
+            <td>john@email</td>
+            <td>9984902811</td>
+          </tr>
+          <tr :key="ewrtuyewa">
+            <td>John</td>
+            <td>john@email</td>
+            <td>9984902811</td>
+          </tr>
+          <tr :key="csuygdsudao">
+            <td>John</td>
+            <td>john@email</td>
+            <td>9984902811</td>
+          </tr>
+          <tr :key="qwioxsancen">
+            <td>John</td>
+            <td>john@email</td>
+            <td>9984902811</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: [
+          { name: "John Doe", email: "johndoe@example.com", phone: "555-1234" },
+          { name: "Jane Smith", email: "janesmith@example.com", phone: "555-5678" },
+          { name: "Bob Johnson", email: "bobjohnson@example.com", phone: "555-9012" },
+        ],
+      };
+    },
+  };
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+
+import DummyButton from './components/DummyButton.vue'
+import DataListComp from './components/DataListComp.vue'
+
+// export default DummyButton
+// export default DataListComp
+
+import DemoTable from './components/DemoTable.vue'
+export default {
+    DemoTable,
+    DataListComp,
+    DummyButton
+}
